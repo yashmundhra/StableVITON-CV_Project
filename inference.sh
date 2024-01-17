130dc0529e1d4f40b074c2975e0a5edd4dc964f5
Junyoung Shin <jun021222@naver.com>
2024-01-17 04:48:13 +0000

    Update inference.sh

--- inference.sh
+++ inference.sh
@@ -1,15 +1,23 @@
 # VITONHD pair
-CUDA_VISIBLE_DEVICES=3 python inference.py --config_path ./configs/VITON512.yaml --batch_size 1 --model_load_path ./ckpts/VITONHD.ckpt --save_dir VITONHD
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:"/usr/local/cuda-11.7/lib64"
+CUDA_VISIBLE_DEVICES=0 
 
-# VITONHD unpair
-CUDA_VISIBLE_DEVICES=3 python inference.py --config_path ./configs/VITON512.yaml --batch_size 1 --model_load_path ./ckpts/VITONHD.ckpt --unpair --save_dir VITONHD
+python inference.py \
+    --config_path ./configs/VITON512.yaml \
+    --model_load_path ./ckpts/VITONHD.ckpt \
+    --save_dir VITONHD \
+    --batch_size 1 \
+    --unpair
 
-# VITONHD pair repaint
-CUDA_VISIBLE_DEVICES=3 python inference.py --config_path ./configs/VITON512.yaml --batch_size 1 --model_load_path ./ckpts/VITONHD.ckpt --repaint --save_dir VITONHD_repaint
+# # VITONHD unpair
+# CUDA_VISIBLE_DEVICES=3 python inference.py --config_path ./configs/VITON512.yaml --batch_size 1 --model_load_path ./ckpts/VITONHD.ckpt --unpair --save_dir VITONHD
 
-# VITONHD unpair repaint
-CUDA_VISIBLE_DEVICES=3 python inference.py --config_path ./configs/VITON512.yaml --batch_size 1 --model_load_path ./ckpts/VITONHD.ckpt --unpair --repaint --save_dir VITONHD_repaint
+# # VITONHD pair repaint
+# CUDA_VISIBLE_DEVICES=3 python inference.py --config_path ./configs/VITON512.yaml --batch_size 1 --model_load_path ./ckpts/VITONHD.ckpt --repaint --save_dir VITONHD_repaint
+
+# # VITONHD unpair repaint
+# CUDA_VISIBLE_DEVICES=3 python inference.py --config_path ./configs/VITON512.yaml --batch_size 1 --model_load_path ./ckpts/VITONHD.ckpt --unpair --repaint --save_dir VITONHD_repaint
 
 
-# dummy
-CUDA_VISIBLE_DEVICES=5 python inference.py --config_path ./configs/VITON512.yaml --batch_size 1 --model_load_path ./ckpts/VITONHD.ckpt --save_dir samples_new2
+# # dummy
+# CUDA_VISIBLE_DEVICES=5 python inference.py --config_path ./configs/VITON512.yaml --batch_size 1 --model_load_path ./ckpts/VITONHD.ckpt --save_dir samples_new2
