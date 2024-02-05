735489bcf7d997dc1147228e0611e2b215a99906
Junyoung Shin <jun021222@naver.com>
2024-02-05 06:53:00 +0000

    Add train augmentation code

---
+++ aug/augmentation.py
@@ -0,0 +1,30 @@
+import albumentations as A
+from cv2 import BORDER_CONSTANT
+
+# Applied to clothing and U-Net's input condition
+transform_flip = A.Compose(
+    [A.HorizontalFlip(p=0.5)],
+    additional_targets={
+        'agn': 'image',
+        'agn_mask': 'mask',
+        'image_densepose': 'mask'
+    }
+)
+
+# Applied to clothing and U-Net's input
+transform_shift_scale = A.Compose([
+    A.ShiftScaleRotate(shift_limit=0.2, scale_limit=0, rotate_limit=0, p=0.5, border_mode=BORDER_CONSTANT),
+    A.ShiftScaleRotate(shift_limit=0, scale_limit=0.2, rotate_limit=0, p=0.5, border_mode=BORDER_CONSTANT)],
+    additional_targets={
+        'cloth': 'image'
+    }
+)
+
+# Applied to clothing and x_0
+transform_color = A.Compose([
+    A.HueSaturationValue(hue_shift_limit=5, sat_shift_limit=5, val_shift_limit=5, p=0.5),
+    A.RandomBrightnessContrast(brightness_limit=0, contrast_limit=0.3, p=0.5)],
+    additional_targets={
+        'cloth': 'image'
+    }
+)
--- dataset.py
+++ dataset.py
@@ -1,23 +1,37 @@
 from os.path import join as opj
 
-import cv2
+import cv2 as cv
 import numpy as np
 from torch.utils.data import Dataset
 
+from aug.augmentation import *
+
 def imread(p, h, w, is_mask=False, in_inverse_mask=False, img=None):
     if img is None:
-        img = cv2.imread(p)
+        img = cv.imread(p)
     if not is_mask:
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        img = cv2.resize(img, (w,h))
+        img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+        img = cv.resize(img, (w,h))
         img = (img.astype(np.float32) / 127.5) - 1.0  # [-1, 1]
     else:
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        img = cv2.resize(img, (w,h))
+        img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+        img = cv.resize(img, (w,h))
         img = (img >= 128).astype(np.float32)  # 0 or 1
         img = img[:,:,None]
         if in_inverse_mask:
             img = 1-img
+    return img
+
+def image_int_to_float(img, is_mask=False, invert_mask=False):
+    if is_mask:
+        img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+        img = (img >= 128).astype(np.float32)
+        img = img[:,:,None]
+        if invert_mask:
+            img = 1 - img
+    else:
+        img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+        img = (img.astype(np.float32) / 127.5) - 1.0
     return img
 
 class VITONHDDataset(Dataset):
@@ -60,12 +74,33 @@ class VITONHDDataset(Dataset):
     def __getitem__(self, idx):
         img_fn = self.im_names[idx]
         cloth_fn = self.c_names[self.pair_key][idx]
-        agn = imread(opj(self.drd, self.data_type, "agnostic-v3.2", self.im_names[idx]), self.img_H, self.img_W)
-        agn_mask = imread(opj(self.drd, self.data_type, "agnostic-mask", self.im_names[idx].replace(".jpg", "_mask.png")), self.img_H, self.img_W, is_mask=True, in_inverse_mask=True)
-        cloth = imread(opj(self.drd, self.data_type, "cloth", self.c_names[self.pair_key][idx]), self.img_H, self.img_W)
 
-        image = imread(opj(self.drd, self.data_type, "image", self.im_names[idx]), self.img_H, self.img_W)
-        image_densepose = imread(opj(self.drd, self.data_type, "image-densepose", self.im_names[idx]), self.img_H, self.img_W)
+        agn = cv.imread(opj(self.drd, self.data_type, "agnostic-v3.2", self.im_names[idx]))
+        agn_mask = cv.imread(opj(self.drd, self.data_type, "agnostic-mask", self.im_names[idx].replace(".jpg", "_mask.png")))
+
+        cloth = cv.imread(opj(self.drd, self.data_type, "cloth", self.c_names[self.pair_key][idx]))
+
+        image = cv.imread(opj(self.drd, self.data_type, "image", self.im_names[idx]))
+        image_densepose = cv.imread(opj(self.drd, self.data_type, "image-densepose", self.im_names[idx]))
+
+        agn, agn_mask, cloth, image, image_densepose =\
+            map(lambda img: cv.resize(img, (self.img_W, self.img_H)), [agn, agn_mask, cloth, image, image_densepose])
+        
+        if not self.is_test: # train
+            aug = transform_flip(image=cloth , agn=agn, agn_mask=agn_mask, image_densepose=image_densepose)
+            cloth, agn, agn_mask, image_densepose = \
+                aug['image'], aug['agn'], aug['agn_mask'], aug['image_densepose']
+            
+            aug = transform_shift_scale(image=image, cloth=cloth)
+            cloth, image = aug['cloth'], aug['image']
+
+            aug = transform_color(image=image, cloth=cloth)
+            cloth, image = aug['cloth'], aug['image']
+        
+        agn, cloth, image, image_densepose = \
+            map(image_int_to_float, [agn, cloth, image, image_densepose])
+        agn_mask = image_int_to_float(agn_mask, is_mask=True, invert_mask=True)
+        
         return dict(
             agn=agn,
             agn_mask=agn_mask,
